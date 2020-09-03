0c9ac5eda06965090531bb5ad6847470c37b160c
Eduardo Mass <emass@asince.com.ar>
2020-09-02 22:06:13 -0300

    no message

--- Juegos/TATETI/tateti.js
+++ Juegos/TATETI/tateti.js
@@ -1,24 +1,58 @@
-
-function PonerXoY() {
+let turno = 'X';
+const vacio = '?';
+function PonerXoY(boton) {
     //Poner la letra que correspona dependiendo de turno
+    boton.value = turno;
 } 
-function ValidarQueEsteVacio() {
+function ValidarQueEsteVacio(boton) {
     //Me dice si donde toco hay o no valor
+    return (boton.value == vacio);
 }
 function CambiarDeTurno() {
     //Si es X pone Y sino, pone X
+    if (turno == 'X')
+        turno = 'O';
+    else
+        turno = 'X';
 }
-function ValidarSiAlguienGano() {
+function ValidarTodosIgualesXY(btn1, btn2, btn3) {
+    let btnA = document.getElementById(btn1).value;
+    let btnB = document.getElementById(btn2).value;
+    let btnC = document.getElementById(btn3).value;
 
+    if (btnA == btnB && btnB == btnC && btnA != '?') {
+        document.getElementById(btn1).style.background = 'red';
+        return btnA;
+    }
+    return '';
 }
-function Jugar(carlos) {
-    alert(carlos.getAttribute('fila'));
-    //boton.value = 'X';
-    if (!ValidarQueEsteVacio()) {
+
+function ValidarSiAlguienGano() {
+    let forma = ValidarTodosIgualesXY('btn11', 'btn12', 'btn13');
+    forma += ValidarTodosIgualesXY('btn21', 'btn22', 'btn23');
+    forma += ValidarTodosIgualesXY('btn31', 'btn32', 'btn33');
+    forma += ValidarTodosIgualesXY('btn11', 'btn21', 'btn31');
+    forma += ValidarTodosIgualesXY('btn12', 'btn22', 'btn32');
+    forma += ValidarTodosIgualesXY('btn13', 'btn23', 'btn33');
+    forma += ValidarTodosIgualesXY('btn11', 'btn22', 'btn33');
+    forma += ValidarTodosIgualesXY('btn13', 'btn22', 'btn31');
+    return forma;
+}
+function Jugar(boton) {
+
+    let quienganoAntesDeTocar = ValidarSiAlguienGano();
+    if (quienganoAntesDeTocar != '')
+        return;
+    if (!ValidarQueEsteVacio(boton)) {
         alert('dale heramno toca en un ?!');
         return;
     }
-    PonerXoY(carlos);
-    ValidarSiAlguienGano();
-    CambiarDeTurno();
+    PonerXoY(boton);
+    let quiengano = ValidarSiAlguienGano();
+    if (quiengano == '')
+        CambiarDeTurno();
+    else {
+        alert('Juego terminado, gano ' + quiengano);
+        
+    }
 }
