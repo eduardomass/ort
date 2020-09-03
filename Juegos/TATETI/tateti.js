3a6c5540de2bd0d32b2fd80510dff8419fcbea5d
Eduardo Mass <emass@asince.com.ar>
2020-09-02 21:02:31 -0300

    Tateti

--- Juegos/TATETI/tateti.js
+++ Juegos/TATETI/tateti.js
@@ -1,1 +1,24 @@
 
+function PonerXoY() {
+    //Poner la letra que correspona dependiendo de turno
+} 
+function ValidarQueEsteVacio() {
+    //Me dice si donde toco hay o no valor
+}
+function CambiarDeTurno() {
+    //Si es X pone Y sino, pone X
+}
+function ValidarSiAlguienGano() {
+
+}
+function Jugar(carlos) {
+    alert(carlos.getAttribute('fila'));
+    //boton.value = 'X';
+    if (!ValidarQueEsteVacio()) {
+        alert('dale heramno toca en un ?!');
+        return;
+    }
+    PonerXoY(carlos);
+    ValidarSiAlguienGano();
+    CambiarDeTurno();
+}
